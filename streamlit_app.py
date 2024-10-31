4ee7304cc90ec02c66774148ffd8497a040670ec
rogervp <roger.vila@sdggroup.com>
2024-10-31 11:18:33 +0100

    Update streamlit_app.py
    Added .json() format to request

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,4 @@ if ingredients_list:
 #Section to display fruit information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
