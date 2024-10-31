4f59aa0f1110907011f1a3504af3ead24b3fa77a
rogervp <roger.vila@sdggroup.com>
2024-10-31 11:02:18 +0100

    Update streamlit_app.py
    Commented requests because there is an error and will not be used

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
-#Section to display fruit information
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+##Section to display fruit information
+#import requests
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#st.text(fruityvice_response)
